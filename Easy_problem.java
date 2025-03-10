4dfc6ab7c24fea4e63d91b8f2ba9c25554b0620c
Adarsh1901 <sharma190103adarsh@gmail.com>
2025-03-10 09:42:12 +0530

    Create Easy_problem.java

---
+++ Easy_problem.java
@@ -0,0 +1,32 @@
+import java.sql.*;
+
+public class MySQLConnection {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/your_database_name";
+        String username = "your_username";
+        String password = "your_password";
+        String query = "SELECT EmpID, Name, Salary FROM Employee";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(url, username, password);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+                int empID = resultSet.getInt("EmpID");
+                String name = resultSet.getString("Name");
+                double salary = resultSet.getDouble("Salary");
+                System.out.println("EmpID: " + empID + ", Name: " + name + ", Salary: " + salary);
+            }
+
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
